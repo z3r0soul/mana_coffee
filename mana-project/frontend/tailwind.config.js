3b6538c6795072bd049b69aa06f372ce266d6e0d
z3r0soul <santvargasg@udistrital.edu.co>
2025-11-30 11:15:42 -0500

    cambio comentarios

--- mana-project/frontend/tailwind.config.js
+++ mana-project/frontend/tailwind.config.js
@@ -6,8 +6,8 @@ export default {
     theme: {
         extend: {
             colors: {
-                'mana-brown': '#5D4037', // Placeholder: Replace with actual color
-                'mana-gold': '#FFD700',  // Placeholder: Replace with actual color
+                'mana-brown': '#5D4037',
+                'mana-gold': '#FFD700',
             }
         },
     },
