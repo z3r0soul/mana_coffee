d9cd98bc15271c38583bc7781b124a5feccdb620
z3r0soul <santvargasg@udistrital.edu.co>
2025-12-02 21:13:16 -0500

    Actualizacion en App.jsx
    Así añadimos la ruta para testear el home

--- mana-project/frontend/src/App.jsx
+++ mana-project/frontend/src/App.jsx
@@ -8,7 +8,7 @@ function App() {
     <BrowserRouter>
       <Navbar />
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Home />} /> /* Home */
       </Routes>
     </BrowserRouter>
   );
