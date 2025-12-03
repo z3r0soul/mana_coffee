64bffa12a56deb6a62dfc1072ea9d8f1997b7278
Santiago Vargas Gomez <santvargasg@udistrital.edu.co>
2025-12-02 21:20:35 -0500

    Add routing with Home component to App

--- mana-project/frontend/src/App.jsx
+++ mana-project/frontend/src/App.jsx
@@ -1,13 +1,15 @@
 import { BrowserRouter } from 'react-router-dom';
 import Navbar from './components/Navbar';
+import { Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
 
 function App() {
   return (
     <BrowserRouter>
       <Navbar />
-      <div className="mt-20 p-10">
-        <h1 className="text-3xl font-bold text-mana-brown">Probando el Navbar</h1>
-      </div>
+      <Routes>
+        <Route path="/" element={<Home />} /> /* Home */
+      </Routes>
     </BrowserRouter>
   );
 }
